52a832bcbd136d50eadbea49a80293c20a65a394
naorzkall <kallnaorz@gmail.com>
2025-03-12 17:47:52 +0300

    Type Inference & Type Casting (5)

--- app.ts
+++ app.ts
@@ -1,10 +1,18 @@
+
+const num1Element = document.getElementById('num1') as HTMLInputElement;
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+const btnElement = document.querySelector('button');
+
+
 function add(num1:number,num2:number):number {
     return num1+num2;
 }
 
-console.log(add(1,6));
-
-// ide gona give error
-// console.log(add("1","6"));
+btnElement?.addEventListener('click',() => {
+    const num1  = num1Element.value;
+    const num2  = num2Element.value;
+    const result = add(+num1,+num2);
+    console.log(result);
+});
 
 
