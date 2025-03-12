1e541bcf93f139ed534749120a038154554c0785
naorzkall <kallnaorz@gmail.com>
2025-03-12 22:10:40 +0200

    Working With Union Types

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-var num1Element = document.getElementById('num1');
-var num2Element = document.getElementById('num2');
-var btnElement = document.querySelector('button');
+"use strict";
+const num1Element = document.getElementById('num1');
+const num2Element = document.getElementById('num2');
+const btnElement = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2; // add
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return num1 + " " + num2; // concatination
+    }
+    else {
+        return +num1 + +num2; // casting
+    }
 }
-btnElement === null || btnElement === void 0 ? void 0 : btnElement.addEventListener('click', function () {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2);
+btnElement.addEventListener('click', () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(num1, num2);
     console.log(result);
 });
+console.log(add(5, 11)); // 16
+console.log(add("Hello", "World")); // "Hello World"
+console.log(add("5", 11)); // 16
--- app.ts
+++ app.ts
@@ -1,18 +1,27 @@
 
 const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
-const btnElement = document.querySelector('button');
+const btnElement = document.querySelector('button')!;
 
 
-function add(num1:number,num2:number):number {
-    return num1+num2;
-}
+function add(num1: number | string, num2: number | string) {
+    if (typeof num1 === "number" && typeof num2 === "number") {
+      return num1 + num2; // add
+    } else if (typeof num1 === "string" && typeof num2 === "string") {
+      return num1 + " " + num2; // concatination
+    } else {
+      return +num1 + +num2; // casting
+    }
+  }
+  
 
-btnElement?.addEventListener('click',() => {
+btnElement.addEventListener('click',() => {
     const num1  = num1Element.value;
     const num2  = num2Element.value;
-    const result = add(+num1,+num2);
+    const result = add(num1,num2);
     console.log(result);
 });
 
-
+console.log(add(5, 11));       // 16
+console.log(add("Hello", "World"));  // "Hello World"
+console.log(add("5", 11));     // 16
